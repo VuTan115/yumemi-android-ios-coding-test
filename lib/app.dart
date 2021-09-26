dede4978535d65853992df62ad98517ef8c6df63
VuTan115 <tanboy042001@gmail.com>
2021-09-26 16:33:11 +0700

    initial clean code base

---
+++ lib/app.dart
@@ -0,0 +1,16 @@
+import 'package:android_ios_test/pages/home.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: Home());
+  }
+}
